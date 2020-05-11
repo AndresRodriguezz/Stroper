11294c832124ba4eb881f4f60d1d2b6604c2658d
Andres <jarodriguezh0@gmail.com>
2020-05-11 14:46:02 -0500

    Se agrego Indicador de puntos en el swipeView de ayuda y se le agrego el boton terminar al swipeVie

--- app/src/main/java/co/and/strooper/activities/ContenedorInstruccionesActivity.java
+++ app/src/main/java/co/and/strooper/activities/ContenedorInstruccionesActivity.java
@@ -1,5 +1,6 @@
 package co.and.strooper.activities;
 
+import android.net.sip.SipSession;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,14 +10,20 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import co.and.strooper.R;
 import co.and.strooper.activities.ui.main.SectionsPagerAdapter;
 
 public class ContenedorInstruccionesActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    private LinearLayout linearPuntos;
+    private TextView[] puntosSlide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +31,58 @@ public class ContenedorInstruccionesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contenedor_instrucciones);
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
 
+        linearPuntos = findViewById(R.id.idLinearPuntos);
+        //Meotodo que carga con la creacion del activity dandole el valor de cero despues se va al onChangeListener para cuando se mueva de fragment
+        agregarIndicadorPuntos(0);
+        //Agregamos la referencia al PageChangeListener
+        viewPager.addOnPageChangeListener(viewListener);
+
+
     }
+
+    private void agregarIndicadorPuntos(int pos) {
+        //Se le asigna el tamapao a nuestro arreglo de tipo TextView no olvidar el casteo
+        puntosSlide = new TextView[8];
+        //Se limpia el linearPuntos anterior
+        linearPuntos.removeAllViews();
+        //Se recorre todo el arreglo
+        for(int i=0;i<puntosSlide.length;i++){
+            //creo los componentes que se van a guardar en el arreglo puntosSlide
+            //Creo la instancia y le digo por defecto cual es la instancia
+            puntosSlide[i]= new TextView(this);
+            //Formato de texto que va utilizar
+            puntosSlide[i].setText(Html.fromHtml("&#8226;"));
+            puntosSlide[i].setTextSize(35);
+            puntosSlide[i].setTextColor(getResources().getColor(R.color.colorBlancoTransparente));
+            //Agregamos nuestro conjunto de views en nuestro linearpuntos
+            linearPuntos.addView(puntosSlide[i]);
+        }
+        if (puntosSlide.length>0){
+            //la posicion que llegue es la que se va a pintar de blanco
+            puntosSlide[pos].setTextColor(getResources().getColor(R.color.colorBlanco));
+        }
+    }
+
+    // implementamos la interfaz OnPageChangeListener
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            //Cuando se mueva a pager 2 es el dato que llevara a position y es el dato que le pasara al metodo agregarIndicadorPuntos
+            agregarIndicadorPuntos(position);
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
--- app/src/main/java/co/and/strooper/fragments/InstruccionFinalFragment.java
+++ app/src/main/java/co/and/strooper/fragments/InstruccionFinalFragment.java
@@ -1,19 +1,28 @@
 package co.and.strooper.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import co.and.strooper.MainActivity;
 import co.and.strooper.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InstruccionFinalFragment extends Fragment {
+    Button btnterminar;
+    View vista;
+    Activity actividad;
 
     public InstruccionFinalFragment() {
         // Required empty public constructor
@@ -24,6 +33,27 @@ public class InstruccionFinalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruccion_final, container, false);
+
+        vista= inflater.inflate(R.layout.fragment_instruccion_final, container, false);
+        btnterminar = (Button)vista.findViewById(R.id.btnTerminar);
+
+        btnterminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Se termina toda la actividad contenedora
+                actividad.finish();
+            }
+        });
+
+        return vista;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        //si el contexto es una instancia de un activity entonces la actividad va a ser igual a ese contexto
+        if(context instanceof Activity){
+            actividad = (Activity) context;
+        }
     }
 }
