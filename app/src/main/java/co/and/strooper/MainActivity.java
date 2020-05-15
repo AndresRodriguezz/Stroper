0b6e5e227a344e3fb714a0199ac585ede2b3eff0
Andres <jarodriguezh0@gmail.com>
2020-05-14 22:13:30 -0500

    Se crea alert Dialoh para gestion de jugadores

--- app/src/main/java/co/and/strooper/MainActivity.java
+++ app/src/main/java/co/and/strooper/MainActivity.java
@@ -1,10 +1,12 @@
 package co.and.strooper;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import co.and.strooper.activities.ContenedorInstruccionesActivity;
 import co.and.strooper.interfaces.iComunicarFragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -15,6 +17,29 @@ public class MainActivity extends AppCompatActivity implements iComunicarFragmen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public AlertDialog dialogoGestionDeUsuarios (){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Gestionar Jugador").setMessage("Indique si desea agregar un nuevo jugador"+"o si desea seleccionar uno  ya existente.\n\n"+
+                "Tambien podra modificar un jugador desde la opción SELECCIONAR").setNegativeButton("Registra", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Registrar Jugador", Toast.LENGTH_SHORT).show();
+
+            }
+        }).setPositiveButton("SELECCIONAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Seleccionar Jugador", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        return builder.create();
+
 
     }
 //Metodo que viene desde interface, es un metodo abastractro que se volvio concreto ya que es la clase que la implementa
@@ -46,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements iComunicarFragmen
 
     @Override
     public void jugadorJuego() {
-        Toast.makeText(this, "jugador juego desde el MainActivity", Toast.LENGTH_SHORT).show();
+        dialogoGestionDeUsuarios().show();
 
     }
 
